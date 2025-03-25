aa8f4494019dac0d697ef8bd9c1a9c3c0bc40088
kiyo <18kyohei@gmail.com>
2025-03-25 18:51:45 +0900

    Registrations Controllerの作成を行った。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :phone_number, :birthdate, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :phone_number, :birthdate, :password, :password_confirmation,
+                                 :current_password)
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
