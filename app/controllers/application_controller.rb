705cefd60400989216ca9525abbd613ab0a41c2a
kiyo <18kyohei@gmail.com>
2025-03-27 18:54:28 +0900

    コンフリクトの解消 ログインまたはsign_inした後の処理を記述した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,14 @@ class ApplicationController < ActionController::Base
   end
   # rubocop:enable Hc/RailsSpecificActionName
 
+  def after_sign_in_path_for(_resource)
+    root_path  # トップページにリダイレクト
+  end
+
+  def after_sign_up_path_for(_resource)
+    root_path
+  end
+
   protected
 
   def configure_permitted_parameters
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 # frozen_string_literal: true
 
+=======
+>>>>>>> e3c3d1e (ログインまたはsign_inした後の処理を記述した。)
 module HomeHelper
 end
