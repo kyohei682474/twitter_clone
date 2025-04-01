e05506fa55fc70e586ca54b8d37b90b7bb394c16
kiyo <18kyohei@gmail.com>
2025-04-01 12:49:32 +0900

    linkにgithubを使用したログインまたは新規登録に遷移するURLを挿入した

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,8 +5,8 @@ module Users
     def github
       @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format? # HTMLを使用した画面遷移の時のみフラッシュメッセージを挿入。 # rubocop:disable Layout/LineLength
       else
         redirect_to new_user_registration_url, alert: 'GitHubでの認証に失敗しました。'
       end
