66865905cf9e87022babfafa29aaf4be65ba3f50
kiyo <18kyohei@gmail.com>
2025-04-01 12:13:48 +0900

    コンフリクトの解消 user.rbファイルにGitHubを使用して認証行う処理を実装した

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   validates :birthdate, presence: true, unless: :github_login?
   validate :birthdate_cannot_be_in_the_future
   # Githubユーザー情報をもとに既存のユーザーを検索または新規作成を行う。以前にもGitHubを使用して認証したことがあるか、初めてログインした人かを判別
+  # あくまでGitHubを使用して認証した場合の処理なのでbirthdateやphone_numberは記述しない
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email || "#{auth.uid}github.com" # GitHubがメールアドレスを提供しない場合は,uidを使用する
