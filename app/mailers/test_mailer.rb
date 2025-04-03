59fabc568dcd5e5f77a85c1a4223f0794f090c87
kiyo <18kyohei@gmail.com>
2025-04-04 06:20:20 +0900

    コンフリクトの解消

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,20 +1,9 @@
 class TestMailer < ApplicationMailer
-<<<<<<< HEAD
   def send_email(to)
     mail(
       to: to,
       from: 'MS_VJC9Yl@test-nrw7gym906rg2k8e.mlsender.net',
       subject: 'Test Mail from Mailersend'
-=======
-  default from: '18kyohei@sandboxe0a8b94f9d0e4071886f4e8c46377961.mailgun.org'
-
-  def send_email(to)
-    mail(
-      to: to,
-      from: ENV['MAILGUN_SMTP_LOGIN'],
-      subject: 'Test Mail',
-      body: 'GitHub認証メール'
->>>>>>> 0dd7096 (本番環境用のメールの設定を行った)
     )
   end
 end
