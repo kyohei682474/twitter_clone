c03ce7f34c625c6e584e03b232c2df8be6e9d589
kiyo <18kyohei@gmail.com>
2025-04-04 16:51:14 +0900

    test_mailerの変更を行った

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -2,7 +2,7 @@ class TestMailer < ApplicationMailer
   def send_email(to)
     mail(
       to: to,
-      from: 'MS_VJC9Yl@test-nrw7gym906rg2k8e.mlsender.net',
+      from: ENV['DEFAULT_FROM_EMAIL'],
       subject: 'Test Mail from Mailersend'
     )
   end
