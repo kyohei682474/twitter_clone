6a9d0ba2c51b2e73f00731b3756ad790e1e37508
kiyo <18kyohei@gmail.com>
2025-04-05 19:08:00 +0900

    GitHubでログイン後、メールが転送されるシステムを実装した

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,8 +5,10 @@ module Users
     def github
       @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user.persisted?
+        UserMailer.welcome_email(@user).deliver_latrer
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format? # HTMLを使用した画面遷移の時のみフラッシュメッセージを挿入。 # rubocop:disable Layout/LineLength
+
       else
         redirect_to new_user_registration_url, alert: 'GitHubでの認証に失敗しました。'
       end
