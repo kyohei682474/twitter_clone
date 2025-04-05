2fa1f818f3e3b6c8800f1d29e533eff8c9f58637
kiyo <18kyohei@gmail.com>
2025-04-05 19:57:08 +0900

    undefined methodを修正した

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ module Users
     def github
       @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user.persisted?
-        ::TestMailer.welcome_email(@user).deliver_latrer
+        ::TestMailer.send_email(@user).deliver_latrer
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format? # HTMLを使用した画面遷移の時のみフラッシュメッセージを挿入。 # rubocop:disable Layout/LineLength
 
