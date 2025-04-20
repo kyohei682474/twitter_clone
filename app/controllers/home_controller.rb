da56297cf5f528c6986b77bf6b39d9acd93dfefe
kiyo <18kyohei@gmail.com>
2025-04-21 08:28:54 +0900

    ツイート一覧が出力されないので一旦戻した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,6 @@ class HomeController < ApplicationController
   end
 
   def following
-    @user = current_user
     @tweets = Tweet.where(user_id: current_user.followings.ids)
                    .includes(:user)
                    .order(created_at: :desc)
