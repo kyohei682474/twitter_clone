fb9bef3889b4d7e83abc191379ae29186f4ee451
kiyo <18kyohei@gmail.com>
2025-04-23 17:49:36 +0900

    robocopに関する修正を行なった

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,5 +3,5 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
-  validates :user_id, uniqueness: { scope: :tweet_id, message: 'has already liked this tweet' }
+  validates :user_id, uniqueness: { scope: :tweet_id }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_17_220219) do
+ActiveRecord::Schema[7.0].define(version: 2025_04_23_084242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_17_220219) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["tweet_id"], name: "index_retweets_on_tweet_id"
+    t.index ["user_id", "tweet_id"], name: "index_retweets_on_user_id_and_tweet_id", unique: true
     t.index ["user_id"], name: "index_retweets_on_user_id"
   end
 
