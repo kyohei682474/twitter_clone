9f3c9eec256f49689aaf76f4d60fb97e7c1afcac
kiyo <18kyohei@gmail.com>
2025-04-25 14:43:16 +0900

    編集画面を作成した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,15 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'プロフィールを更新しました'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
