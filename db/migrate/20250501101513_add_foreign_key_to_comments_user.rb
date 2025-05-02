573a8ec32b01e798988ba0160a5f5f801a39c896
kiyo <18kyohei@gmail.com>
2025-05-03 06:36:37 +0900

    rubocopとhtmlbeautifierを通した

--- db/migrate/20250501101513_add_foreign_key_to_comments_user.rb
+++ db/migrate/20250501101513_add_foreign_key_to_comments_user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddForeignKeyToCommentsUser < ActiveRecord::Migration[7.0]
   def change
     remove_foreign_key :comments, :users if foreign_key_exists?(:comments, :users)
