67449bbc2455b2dda6563fb99e74783563484a3a
kiyo <18kyohei@gmail.com>
2025-05-04 08:18:07 +0900

    いいね機能を実装してhtmlに反映させた

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -12,5 +12,12 @@ class LikesController < ApplicationController
 
   def destroy
     @tweet = Tweet.find(params[:tweet_id])
+    @like = current_user.likes.find_by(tweet: @tweet)
+    if @like.destroy
+      flash[:notice] = 'いいねを解除しました'
+      redirect_to tweet_path(@tweet)
+    else
+      flash[:alert] = 'いいねの解除に失敗しました'
+    end
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -11,4 +11,12 @@ class Tweet < ApplicationRecord
   validates :body, presence: true, length: { maximum: 140 }
 
   paginates_per 5
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
+
+  def liked_from(user)
+    likes.find_by(user_id: user.id)
+  end
 end
