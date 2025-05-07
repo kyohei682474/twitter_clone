63cc48cd0ab2ecfe7f2d680685c046114853b493
kiyo <18kyohei@gmail.com>
2025-05-08 07:17:52 +0900

    rubocopにかけて修正を行った。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class LikesController < ApplicationController
   def create
     @tweet = Tweet.find(params[:tweet_id])
--- config/initializers/turbo_patch.rb
+++ config/initializers/turbo_patch.rb
@@ -1,18 +1,19 @@
-Rails.application.config.after_initialize do
-  module TurboMethodPatch
-    def link_to(name = nil, options = nil, html_options = nil, &block)
-      html_options ||= {}
+# frozen_string_literal: true
 
-      # method指定があり、data-turbo-method が未指定の場合
-      if html_options[:method] && (html_options[:data].nil? || !html_options[:data].key?(:turbo_method))
-        html_options[:data] ||= {}
-        html_options[:data][:turbo_method] = html_options[:method]
-        html_options.delete(:method)
-      end
+module TurboMethodPatch
+  def link_to(name = nil, options = nil, html_options = nil, &block)
+    html_options ||= {}
 
-      super(name, options, html_options, &block)
+    if html_options[:method] && (html_options[:data].nil? || !html_options[:data].key?(:turbo_method))
+      html_options[:data] ||= {}
+      html_options[:data][:turbo_method] = html_options[:method]
+      html_options.delete(:method)
     end
-  end
 
+    super(name, options, html_options, &block)
+  end
+end
+
+Rails.application.config.after_initialize do
   ActionView::Helpers::UrlHelper.prepend(TurboMethodPatch)
 end
