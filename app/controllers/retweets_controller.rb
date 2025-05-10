e72638d3d7cae3207353f4f43ced962f1e9d96a1
kiyo <18kyohei@gmail.com>
2025-05-10 22:21:06 +0900

    簡易的なリツイートはできたが、きちんと動作しているか不明

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -2,7 +2,10 @@ class RetweetsController < ApplicationController
   before_action :authenticate_user!
   def create
     original = Tweet.find(params[:tweet_id]) # リツイート元のツイートを取得
-    current_user.tweets.build(retweeted_from: original) unless current_user.tweets.exists?(retweeted_from: original)
+    unless current_user.tweets.exists?(retweeted_from: original)
+      retweet = current_user.tweets.build(retweeted_from: original)
+    end
+    retweet.save # リツイートを保存
     redirect_to root_path, notice: 'リツイートしました'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'retweets/create'
-  get 'retweets/destroy'
   root 'home#index' # トップページに遷移させる
   get 'following', to: 'home#following'
   resources :tweets, only: %i[new create show]
