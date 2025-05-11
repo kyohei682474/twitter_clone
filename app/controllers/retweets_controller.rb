dea1c3bea23e2af1504f99897b100d056f8777b8
kiyo <18kyohei@gmail.com>
2025-05-11 11:37:49 +0900

    リツイート時、リツイート解除時にフラッシュメッセージが表示できるようにした

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -7,20 +7,27 @@ class RetweetsController < ApplicationController
     @retweet = current_user.tweets.find_or_initialize_by(retweeted_from: original)
     return head :unprocessable_entity if @retweet.new_record? && !@retweet.save
 
+    flash.now[:notice] = 'リツイートしました'
     respond_to do |format|
       format.turbo_stream
-      flash[:notice] = 'リツイートしました'
     end
   end
 
   def destroy
     # カレントユーザのリツイートした内容を削除する
     @retweet = current_user.tweets.find_by(retweeted_from_id: params[:id])
-    if @retweet
-      @retweet.destroy
-      redirect_to root_path, notice: 'リツイートを解除しました'
-    else
-      redirect_to root_path, alert: 'リツイートが見つかりませんでした'
+    return head :not_found unless @retweet
+
+    @retweet.destroy
+    # if @retweet
+    #   @retweet.destroy
+    #   redirect_to root_path, notice: 'リツイートを解除しました'
+    # else
+    #   redirect_to root_path, alert: 'リツイートが見つかりませんでした'
+    # end
+    flash.now[:alert] = 'リツイートを解除しました'
+    respond_to do |format|
+      format.turbo_stream
     end
   end
 end
