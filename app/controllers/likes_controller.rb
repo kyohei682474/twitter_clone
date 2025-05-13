4f0641581c805d311a5556565ff1a749e7eb5a32
kiyo <18kyohei@gmail.com>
2025-05-14 07:15:58 +0900

    いいねの非同期処理ができたがレイアウトが崩れている

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,21 +4,22 @@ class LikesController < ApplicationController
   def create
     @tweet = Tweet.find(params[:tweet_id])
     @like = current_user.likes.build(tweet: @tweet)
-    if @like.save
-      flash[:notice] = 'いいねしました'
-      redirect_to tweet_path(@tweet)
-    else
-      flash[:alert] = 'いいねいに失敗しました'
-    end
     # if @like.save
-    #   respond_to do |format|
-    #     format.turbo_stream
-    #     format.html { redirect_back fallback_location: root_path, notice: 'いいねしました' }
-    #   end
+    #   flash[:notice] = 'いいねしました'
+    #   redirect_to tweet_path(@tweet)
     # else
-    #   flash[:alert] = 'いいねに失敗しました'
-    #   redirect_back fallback_location: root_path
+    #   flash[:alert] = 'いいねいに失敗しました'
     # end
+    if @like.save
+      respond_to do |format|
+        format.turbo_stream
+        flash.now[:notice] = 'いいねしました'
+        format.html { redirect_to tweet_path(@tweet) }
+      end
+    else
+      flash[:alert] = 'いいねに失敗しました'
+      redirect_back fallback_location: root_path
+    end
   end
 
   def destroy
