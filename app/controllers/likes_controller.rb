3611494d9dfc1fd2f751018c16d38f37f8439ca8
kiyo <18kyohei@gmail.com>
2025-05-15 18:54:54 +0900

    いいねの解除と実行を非同期で行うことができるようになった

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -20,11 +20,19 @@ class LikesController < ApplicationController
   def destroy
     @tweet = Tweet.find(params[:tweet_id])
     @like = current_user.likes.find_by(tweet: @tweet)
-    if @like.destroy
-      flash[:notice] = 'いいねを解除しました'
-      redirect_to tweet_path(@tweet)
+
+    if @like&.destroy
+      flash.now[:notice] = 'いいねを解除しました'
+      respond_to do |format|
+        format.turbo_stream { render 'likes/destroy' }
+        format.html { redirect_to tweet_path(@tweet) }
+      end
     else
-      flash[:alert] = 'いいねの解除に失敗しました'
+      flash.now[:alert] = 'いいねの解除に失敗しました'
+      respond_to do |format|
+        format.turbo_stream { render 'likes/destroy' }
+        format.html { redirect_to tweet_path(@tweet) }
+      end
     end
   end
 end
