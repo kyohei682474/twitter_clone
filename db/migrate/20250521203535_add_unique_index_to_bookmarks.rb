4a9e25a34a881e98f7fa3c92f7f94f3c50d69fe7
kiyo <18kyohei@gmail.com>
2025-05-23 18:56:54 +0900

    ブックマーク一覧を表示できるように実装した

--- db/migrate/20250521203535_add_unique_index_to_bookmarks.rb
+++ db/migrate/20250521203535_add_unique_index_to_bookmarks.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddUniqueIndexToBookmarks < ActiveRecord::Migration[7.0]
   def change
     reversible do |dir|
--- spec/requests/bookmarks_spec.rb
+++ spec/requests/bookmarks_spec.rb
@@ -1,25 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Bookmarks", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/bookmarks/create"
+RSpec.describe 'Bookmarks', type: :request do
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/bookmarks/create'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/bookmarks/destroy"
+  describe 'GET /destroy' do
+    it 'returns http success' do
+      get '/bookmarks/destroy'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/bookmarks/index"
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/bookmarks/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
