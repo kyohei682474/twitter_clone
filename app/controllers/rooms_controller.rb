d2c378de200133cfe01c354e446d2ab1f5336616
kiyo <18kyohei@gmail.com>
2025-05-31 18:28:08 +0900

    Revert "rooms/indexを修正を行なった"
    This reverts commit b6d719f20c2a96be58649642c266847fd2bf4eec.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,10 +2,6 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
   def index
     @rooms = current_user.rooms.includes(:chats, :users)
-    @room_user_pairs = current_user.rooms.includes(:users, :chats).map do |room|
-      other_user = room.other_user_for(current_user)
-      [room, other_user]
-    end.uniq { |_, other_user| other_user.id }
   end
 
   def show
@@ -21,8 +17,8 @@ class RoomsController < ApplicationController
     # 自分と相手ユーザーの２人が参加しているルームを探す
     @room = Room.joins(:entries)
                 .where(entries: { user_id: [current_user.id, reception_user.id] })
-                .group('rooms.id')
-                .having('COUNT(DISTINCT entries.user_id) = 2')
+                .group(':room.id')
+                .having('COUNT(DISTINCT entries.user.id) = 2')
                 .first
     # ルームが見つからなければ新規作成
     unless @room
