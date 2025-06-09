c16a63cc742f1442b8be9f78e759ca5f866d42f9
kiyo <18kyohei@gmail.com>
2025-06-09 17:27:07 +0900

    application.helperにaction_name_jpを定義した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
   def hidden_field_tag(_name, _value = nil, _options = {})
     raise 'Happiness chainではhidden_field_tagの使用を禁止しています'
   end
+
+  def action_name_jp(action_type)
+    case action_type
+    when 'like' then 'いいね'
+    when 'comment' then 'コメント'
+    when 'retweet' then 'リツイート'
+    else 'アクション'
+    end
+  end
 end
