3ead4d6bd40401aca773528f163d4559212245e3
kiyo <18kyohei@gmail.com>
2025-06-26 06:58:09 +0900

    モデルスペックのテストを記述している Please enter the commit message for your changes. Lines starting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #名前、メールアドレス、パスワード、電話番号、誕生日があれば有効のユーザー
+  it "is valid with a name, email, password, phone_number and birthday " do
+    user = User.new( name: "Test", 
+                         email: "users123@exapmple.com",
+                         password: "password",
+                         phone_number: "012343333",
+                         birthdate: Date.new(1989, 11, 11)
+                        )
+    expect(user).to be_valid
+  end
+   
+  # 名前が無ければ無効なユーザー
+  # 同じ電話番号のユーザーは無効
+  # 名前が51文字を超えるユーザーは無効
+  # パスワードが5文字のユーザーが無効
 end
