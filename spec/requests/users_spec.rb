dba962692c79839feb8decf902df2d43cfac1992
kiyo <18kyohei@gmail.com>
2025-06-27 19:19:01 +0900

    パスワード入力時の異常系を記述した

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -58,7 +58,7 @@ RSpec.describe 'Users', type: :request do
       end
     end
 
-    # メールアドレスが空欄のとき
+    # 空のemailを送信するとき
     context 'when blank email' do
       let(:invalid_attributes) do
         {
@@ -78,7 +78,7 @@ RSpec.describe 'Users', type: :request do
       end
     end
 
-    # 同じメールアドレスを登録するとき
+    # 同じメールアドレスを送信するとき
     context 'when register the same email' do
       let(:valid_attributes) do
         { name: 'Test User',
@@ -93,6 +93,81 @@ RSpec.describe 'Users', type: :request do
         expect do
           post user_registration_path, params: { user: valid_attributes.merge(email: 'users123@exapmple.com') }
         end.not_to change(User, :count)
+      end
+
+      # 同じemailが含まれているとエラーメッセージが得られる
+      it 'shows error when email is already taken' do
+        post user_registration_path, params: { user: valid_attributes.merge(email: 'users123@exapmple.com') }
+        expect(response.body).to include('メールアドレスはすでに存在します')
+      end
+    end
+
+    # 空のパスワードを送信するとき
+    context 'when send blank password' do
+      let(:invalid_attributes) do
+        { name: 'Test User',
+          email: 'user999@example.com',
+          password: '',
+          phone_number: '01234567890',
+          birthdate: Date.new(1989, 1, 1) }
+      end
+
+      # ユーザーを追加できない
+      it 'does not create user' do
+        expect do
+          post user_registration_path, params: { user: invalid_attributes }
+        end.not_to change(User, :count)
+      end
+    end
+
+    # 6文字未満のパスワードを送信するとき
+    context 'when send less than 6 length passowrd' do
+      let(:invalid_attributes) do
+        { name: 'Test User',
+          email: 'user999@example.com',
+          password: 'passw',
+          phone_number: '01234567890',
+          birthdate: Date.new(1989, 1, 1) }
+      end
+
+      # ユーザ登録できない
+      it 'does not create user' do
+        expect do
+          post user_registration_path, params: { user: invalid_attributes }
+        end.not_to change(User, :count)
+      end
+
+      # パスワードは6文字以上で入力してくださいとメッセージが生じる
+      it 'shows error when password is shorter than 6 characters' do
+        post user_registration_path, params: { user: invalid_attributes }
+        expect(response.body).to include('パスワードは6文字以上で入力してください')
+      end
+    end
+
+    # パスワードと確認用のパスワードが違うとき
+    context 'when password comfirmation does not match' do
+      let(:invalid_attributes) do
+        {
+          name: 'Test User',
+          email: 'user999@example.com',
+          password: 'password',
+          password_confirmation: 'different_password',
+          phone_number: '01234567890',
+          birthdate: Date.new(1989, 1, 1)
+        }
+      end
+
+      # ユーザー登録できない
+      it 'does not create user' do
+        expect do
+          post user_registration_path, params: { user: invalid_attributes }
+        end.not_to change(User, :count)
+      end
+
+      # パスワード確認とパスワードの入力が一致しませんとエラーメッセージが生じる
+      it 'shows error when password confirmation does not match password' do
+        post user_registration_path, params: { user: invalid_attributes }
+        expect(response.body).to include('パスワード確認とパスワードの入力が一致しません')
       end
     end
   end
